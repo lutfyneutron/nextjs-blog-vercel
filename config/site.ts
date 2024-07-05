64ef06def47529d27b303897426a433f5463ba70
lutfiyadi <37740405+lutfyneutron@users.noreply.github.com>
2024-07-05 07:48:50 +0000

    fast time change

---
+++ config/site.ts
@@ -0,0 +1,16 @@
+export const siteConfig = {
+  name: "Lutfiyadi",
+  url: "https://lutfiadi.my.id",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Lutfiyadi",
+  links: {
+    twitter: "https://twitter.com/lutfyneutron",
+    github: "https://github.com/lutfyneutron",
+    personalSite: "https://lutfiadi.my.id",
+    instagram: "https://instagram.com/lutfiadi.my.id",
+    facebook: "https://facebook.com/lutfiadi.my.id",
+    tiktok: "https://www.tiktok.com/@big.d.k",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
