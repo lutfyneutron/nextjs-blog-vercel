8a95d99bff6869ad4260075e38a0c55618ae51cf
lutfiyadi <37740405+lutfyneutron@users.noreply.github.com>
2024-10-29 13:47:07 +0800

    meta-c2

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,8 @@ import { SiteFooter } from "@/components/site-footer";
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
-  name:"google-adsense-account" content="ca-pub-7045075277601936"
+  name:"google-adsense-account",
+  content:"ca-pub-7045075277601936"
 };
 
 export const viewport: Viewport = {
